05b1e871b257f78681d84de70ce361dcc1f9e89e
AnJaeSeong <dktmzh6@gmail.com>
2023-04-13 15:42:56 +0900

    FIX: modify html sanitizeOption
    - li, img, pre, blockquote 태그 허용하도록 sanitize 옵션에 추가

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -3,32 +3,9 @@ import Comment from '../../models/comment';
 import mongoose from 'mongoose';
 import Joi from '../../../node_modules/joi/lib/index';
 import sanitizeHtml from 'sanitize-html';
+import { sanitizeOption } from '../../lib/sanitizeOption';
 
 const { ObjectId } = mongoose.Types;
-
-const sanitizeOption = {
-  allowedTags: [
-    'h1',
-    'h2',
-    'b',
-    'i',
-    'u',
-    's',
-    'p',
-    'ul',
-    'ol',
-    'il',
-    'blockquote',
-    'a',
-    'img',
-  ],
-  allowedAttributes: {
-    a: ['href', 'name', 'target'],
-    img: ['src'],
-    li: ['class'],
-  },
-  allowedSchema: ['data', 'http'],
-};
 
 export const getPostById = async (ctx, next) => {
   const { id } = ctx.params;
@@ -52,7 +29,7 @@ export const getPostById = async (ctx, next) => {
   }
 };
 
-export const write = async ctx => {
+export const write = async (ctx) => {
   // POST /api/posts
   // {
   //     title: '제목',
@@ -63,9 +40,7 @@ export const write = async ctx => {
     // 객체가 다음 필드를 가지고 있음을 검증
     title: Joi.string().required(), // required()가 있으면 필수 항목
     body: Joi.string().required(),
-    tags: Joi.array()
-      .items(Joi.string())
-      .required(), // 문자열로 이루어진 배열
+    tags: Joi.array().items(Joi.string()).required(), // 문자열로 이루어진 배열
   });
 
   // 검증 후 실패 처리
@@ -77,6 +52,7 @@ export const write = async ctx => {
   }
 
   const { title, body, tags } = ctx.request.body;
+
   const post = new Post({
     title,
     body: sanitizeHtml(body, sanitizeOption),
@@ -92,14 +68,14 @@ export const write = async ctx => {
 };
 
 // html을 없애고 내용이 너무 길면 200자로 제한한느 함수
-const removeHtmlAndShorten = body => {
+const removeHtmlAndShorten = (body) => {
   const filtered = sanitizeHtml(body, {
     allowedTags: [],
   });
   return filtered.length < 200 ? filtered : `${filtered.slice(0, 200)}...`;
-}
+};
 
-export const list = async ctx => {
+export const list = async (ctx) => {
   // GET /api/posts
   // query는 문자열이기 때문에 숫자로 변환 필요
   // 값이 주어지지 않았다면 1을 기본으로 사용합니다.
@@ -112,7 +88,7 @@ export const list = async ctx => {
   const { tag, username } = ctx.query;
   // tags username이 유효하면 객체 안에 넣고 아니면 거르기
   const query = {
-    ...(username ? { 'user.username': username } : {}),         //tag username이 없을 경우 {} 빈객체를 할당해주지 않으면 undefined로 해석하고 mongoose가 찾지 못함
+    ...(username ? { 'user.username': username } : {}), //tag username이 없을 경우 {} 빈객체를 할당해주지 않으면 undefined로 해석하고 mongoose가 찾지 못함
     ...(tag ? { tags: tag } : {}),
   };
 
@@ -124,10 +100,10 @@ export const list = async ctx => {
       // .lean()  데이터를 처음부터 mongoose 인스턴스가 아닌 JSON 형태로 불러옴
       .exec();
     const postCount = await Post.countDocuments(query).exec();
-    ctx.set('Last-Page', Math.ceil(postCount / 5));        //커스텀 헤더, 마지막 페이지 정보 저장
+    ctx.set('Last-Page', Math.ceil(postCount / 5)); //커스텀 헤더, 마지막 페이지 정보 저장
     ctx.body = posts
-      .map(post => post.toJSON())         //find로 조회한 데이터는 mongoose 문서 인스턴스 형태이므로 데이터 바로 변형 불가능 -> JSON 형태로 변환한 뒤 조작
-      .map(post => ({
+      .map((post) => post.toJSON()) //find로 조회한 데이터는 mongoose 문서 인스턴스 형태이므로 데이터 바로 변형 불가능 -> JSON 형태로 변환한 뒤 조작
+      .map((post) => ({
         ...post,
         body: removeHtmlAndShorten(post.body),
       }));
@@ -157,7 +133,7 @@ export const read = async (ctx) => {
 //   }
 // }
 
-export const remove = async ctx => {
+export const remove = async (ctx) => {
   // DELETE /api/posts/:id
   const { id } = ctx.params;
   try {
@@ -169,7 +145,7 @@ export const remove = async ctx => {
   }
 };
 
-export const update = async ctx => {
+export const update = async (ctx) => {
   // PATCH /api/posts/:id
   // {
   //     title: '수정',
@@ -193,7 +169,7 @@ export const update = async ctx => {
 
   const nextData = { ...ctx.request.body };
   if (nextData.body) {
-    nextData.body = sanitizeHtml(nextData.body, sanitizeOption)
+    nextData.body = sanitizeHtml(nextData.body, sanitizeOption);
   }
   try {
     const post = await Post.findByIdAndUpdate(id, nextData, {
@@ -212,23 +188,25 @@ export const update = async ctx => {
 
 export const checkOwnPost = (ctx, next) => {
   const { user, post } = ctx.state;
-  if (post.user._id.toString() !== user._id) {     //MongoDB 조회 데이터는 반드시 toString 필요
+  if (post.user._id.toString() !== user._id) {
+    //MongoDB 조회 데이터는 반드시 toString 필요
     ctx.status = 403;
     return;
   }
   return next();
-}
+};
 
 export const checkOwnComment = (ctx, next) => {
   const { user, comment } = ctx.state;
-  if (comment.user._id.toString() !== user._id) {     //MongoDB 조회 데이터는 반드시 toString 필요
+  if (comment.user._id.toString() !== user._id) {
+    //MongoDB 조회 데이터는 반드시 toString 필요
     ctx.status = 403;
     return;
   }
   return next();
-}
+};
 
-export const searchPosts = async ctx => {
+export const searchPosts = async (ctx) => {
   // GET /api/posts/searchPosts
   const page = parseInt(ctx.query.page || '1', 10);
   if (page < 1) {
@@ -249,8 +227,8 @@ export const searchPosts = async ctx => {
     const postCount = await Post.countDocuments(query).exec();
     ctx.set('Last-Page', Math.ceil(postCount / 5));
     ctx.body = post
-      .map(post => post.toJSON())         //find로 조회한 데이터는 mongoose 문서 인스턴스 형태이므로 데이터 바로 변형 불가능 -> JSON 형태로 변환한 뒤 조작
-      .map(post => ({
+      .map((post) => post.toJSON()) //find로 조회한 데이터는 mongoose 문서 인스턴스 형태이므로 데이터 바로 변형 불가능 -> JSON 형태로 변환한 뒤 조작
+      .map((post) => ({
         ...post,
         body: removeHtmlAndShorten(post.body),
       }));
